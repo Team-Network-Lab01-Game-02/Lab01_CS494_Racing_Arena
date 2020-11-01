610f8ae34cb75e0da4f87a1bf9bff1b12895b022
popchim <gooooogle1503@gmai.com>
2020-11-01 16:23:13 +0700

    Added openServer() definition

--- defs/openServer.cpp
+++ defs/openServer.cpp
@@ -1,5 +1,15 @@
 #include "server.h"
 
-int openServer(){
+int openServer(char *portStr){
+  int portno = atoi(portStr);
+  sockaddr_in serverAddr;
+  sockaddr &serverAddrCast = (sockaddr &) serverAddr;
+  int listenFd = socket(AF_INET, SOCK_STREAM, 0);
+  bzero(&serverAddr, sizeof(serverAddr));
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serverAddr.sin_port = htons(1500);
+  bind(listenFd, &serverAddrCast, sizeof(serverAddr));
+  listen(listenFd, portno);
   return 0;
 }
